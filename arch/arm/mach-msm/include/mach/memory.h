thypon/Radioactive-kernel-HAM

[PATCH] Implement kexec-hardboot
    "Allows hard booting (i.e., with a full hardware reboot) to a kernel
    previously loaded in memory by kexec.  This works around the problem of
    soft-booted kernel hangs due to improper device shutdown and/or
    reinitialization."
    More info in /arch/arm/Kconfig.

    Original author: Mike Kasick <mike@kasick.org>

    Vojtech Bocek <vbocek@gmail.com>:
      I've ported it to hammerhead, it is based of my mako/grouper port, which is
      based of Asus TF201 patch ported by Jens Andersen <jens.andersen@gmail.com>

      Hammerhead is the first device I've ported this patch to which uses
      device tree binary instead of atags. It is also rather picky - its
      bootloader erases first 128MB of RAM on reboot, so following hacks had
      to be applied:
      * Copy dtb to location passed in r2 before jumping to kexecd' kernel,
        the kernel would just freeze when I left dtb above 128MB of RAM.
      * Copy kexecd' kernel to location in the first 128MB before jumping to it,
        the decompressor operates very slowly if the kernel is above the cached
        area, copying it lower and decompressing it afterwards is much faster.

      Same as my mako/flo patches, the guest kernel does not require any
      modifications, kexec-hardboot is equivalent to standard kexec.

      To support dtb, following commit was backported from the mainline:
        commit 4cabd1d9625c7d88acd143f4021fbef75394f154
        Author: Matthew Leach <matthew.leach@arm.com>
        Date:   Fri Sep 21 18:56:02 2012 +0100

          ARM: 7539/1: kexec: scan for dtb magic in segments

          This patch allows a dtb to be passed to a new kernel using the kexec
          mechinism.

          When loading segments from userspace, scan each segment's first four
          bytes for the dtb magic. If this is found set the kexec_boot_atags
          parameter to the relocate_kernel code to the phyical address of this
          segment.

    BitO

arch/arm/mach-msm/include/mach/memory.h
@@ -20,6 +20,51 @@
 /* physical offset of RAM */
 #define PLAT_PHYS_OFFSET UL(CONFIG_PHYS_OFFSET)
 
+#if defined(CONFIG_KEXEC_HARDBOOT)
+
+#if defined(CONFIG_MACH_APQ8064_FLO)
+#define KEXEC_HB_PAGE_ADDR		UL(0x88C00000)
+
+#elif defined(CONFIG_MACH_APQ8064_MAKO)
+#define KEXEC_HB_PAGE_ADDR		UL(0x88600000)
+/*
+
+80200000-88dfffff : System RAM
+  80208000-810ad21f : Kernel code
+  8121e000-8163e293 : Kernel data
+
+0x80200000 to 0x88dfffff and the mem-min value I've selected is
+MR_KEXEC_MEM_MIN = 0x85000000.
+*/
+#elif defined(CONFIG_MACH_MSM8974_HAMMERHEAD)
+#define KEXEC_HB_PAGE_ADDR		UL(0x10100000)
+#define KEXEC_HB_KERNEL_LOC		UL(0x3208000)
+
+#elif defined(CONFIG_MACH_OPPO_MSM8974)
+#define KEXEC_HB_PAGE_ADDR		UL(0x2F600000)
+#define KEXEC_HB_KERNEL_LOC		UL(0x3208000)
+/* MR_KEXEC_MEM_MIN := 0x20000000 */
+
+/* ham (ZUK Z1) */
+#elif defined(CONFIG_MACH_SHENQI_K9)
+#define KEXEC_HB_PAGE_ADDR		UL(0x03e00000)
+/* #define KEXEC_HB_KERNEL_LOC		UL(0x3208000) */
+/*
+00000000-045fffff : System RAM
+  00008000-010550eb : Kernel code
+  01216000-016dcb53 : Kernel data
+0d200000-0d3fffff : System RAM
+0d500000-016dcb53 : System RAM
+
+MR_KEXEC_MEM_MIN := 0x03000000
+*/
+
+#else
+#error "Adress for kexec hardboot page not defined"
+
+#endif
+#endif
+
 #define MAX_PHYSMEM_BITS 32
 #define SECTION_SIZE_BITS 28
 

arch/arm/mach-msm/k9/board-8974-k9.c
@@ -52,6 +52,12 @@
 #include "shenqi_nv.h"
 /* Zhenglq, for reading WIFI/BT MAC from NV, END */
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/setup.h>
+#include <asm/memory.h>
+#include <linux/memblock.h>
+#define OPPO_PERSISTENT_RAM_SIZE	(SZ_1M)
+#endif
 
 static struct memtype_reserve msm8974_reserve_table[] __initdata = {
 	[MEMTYPE_SMI] = {
@@ -76,6 +82,26 @@ static struct reserve_info msm8974_reserve_info __initdata = {
 
 void __init msm_8974_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page - just after ram_console,
+	// at the start of second memory bank
+	int ret;
+	phys_addr_t start;
+	struct membank* bank;
+	
+	if (meminfo.nr_banks < 2) {
+		pr_err("%s: not enough membank\n", __func__);
+		return;
+	}
+	
+	bank = &meminfo.bank[1];
+	start = bank->start + SZ_1M + OPPO_PERSISTENT_RAM_SIZE;
+	ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	reserve_info = &msm8974_reserve_info;
 	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8974_reserve_table);
 	msm_reserve();

sb2008/android_kernel_cyanogen_msm8974

arch/arm/mach-msm/include/mach/memory.h
@@ -20,6 +20,52 @@
 /* physical offset of RAM */
 #define PLAT_PHYS_OFFSET UL(CONFIG_PHYS_OFFSET)
 
+#if defined(CONFIG_KEXEC_HARDBOOT)
+
+#if defined(CONFIG_MACH_APQ8064_FLO)
+#define KEXEC_HB_PAGE_ADDR		UL(0x88C00000)
+
+#elif defined(CONFIG_MACH_APQ8064_MAKO)
+#define KEXEC_HB_PAGE_ADDR		UL(0x88600000)
+/*
+
+80200000-88dfffff : System RAM
+  80208000-810ad21f : Kernel code
+  8121e000-8163e293 : Kernel data
+
+0x80200000 to 0x88dfffff and the mem-min value I've selected is
+MR_KEXEC_MEM_MIN = 0x85000000.
+*/
+#elif defined(CONFIG_MACH_MSM8974_HAMMERHEAD)
+#define KEXEC_HB_PAGE_ADDR		UL(0x10100000)
+#define KEXEC_HB_KERNEL_LOC		UL(0x3208000)
+
+#elif defined(CONFIG_MACH_OPPO_MSM8974)
+#define KEXEC_HB_PAGE_ADDR		UL(0x2F600000)
+#define KEXEC_HB_KERNEL_LOC		UL(0x3208000)
+/* MR_KEXEC_MEM_MIN := 0x20000000 */
+
+/* ham (ZUK Z1) */
+#elif defined(CONFIG_MACH_SHENQI_K9)
+#define KEXEC_HB_PAGE_ADDR		UL(0x04400000)
+/*UL(0x03e00000) */
+#define KEXEC_HB_KERNEL_LOC		UL(0x3208000) 
+/*
+00000000-045fffff : System RAM
+  00008000-010550eb : Kernel code
+  01216000-016dcb53 : Kernel data
+0d200000-0d3fffff : System RAM
+0d500000-016dcb53 : System RAM
+
+MR_KEXEC_MEM_MIN := 0x03000000
+*/
+
+#else
+#error "Adress for kexec hardboot page not defined"
+
+#endif
+#endif
+
 #define MAX_PHYSMEM_BITS 32
 #define SECTION_SIZE_BITS 28
 

arch/arm/mach-msm/k9/board-8974-k9.c
@@ -52,6 +52,12 @@
 #include "shenqi_nv.h"
 /* Zhenglq, for reading WIFI/BT MAC from NV, END */
 
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <asm/setup.h>
+#include <asm/memory.h>
+#include <linux/memblock.h>
+#define K9_PERSISTENT_RAM_SIZE	(SZ_1M)
+#endif
 
 static struct memtype_reserve msm8974_reserve_table[] __initdata = {
 	[MEMTYPE_SMI] = {
@@ -76,6 +82,26 @@ static struct reserve_info msm8974_reserve_info __initdata = {
 
 void __init msm_8974_reserve(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page - just after ram_console,
+	// at the start of second memory bank
+	int ret;
+	phys_addr_t start;
+	struct membank* bank;
+	
+	if (meminfo.nr_banks < 2) {
+		pr_err("%s: not enough membank\n", __func__);
+		return;
+	}
+	
+	bank = &meminfo.bank[1];
+	start = bank->start + SZ_1M + K9_PERSISTENT_RAM_SIZE;
+	ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
 	reserve_info = &msm8974_reserve_info;
 	of_scan_flat_dt(dt_scan_for_memory_reserve, msm8974_reserve_table);
 	msm_reserve();